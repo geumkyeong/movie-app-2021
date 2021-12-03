181225fcf407ed6c180404c9db2fcb67333995ff
geumkyeong <ansrmarud@gmail.com>
2021-12-03 17:06:43 +0900

    Updates

---
+++ precache-manifest.898844f3073c7cf29ba8276e9f2b6de0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3e9ebb02cdab8ad30b6b45c55b54151",
+    "url": "/movie-app-2021/index.html"
+  },
+  {
+    "revision": "9796d971800586801b53",
+    "url": "/movie-app-2021/static/js/2.b8806397.chunk.js"
+  },
+  {
+    "revision": "4aa839f25784e6ec59906b932c7e8c9f",
+    "url": "/movie-app-2021/static/js/2.b8806397.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad08e997a3415a5d76de",
+    "url": "/movie-app-2021/static/js/main.9333271f.chunk.js"
+  },
+  {
+    "revision": "7cd2695ee5169c4d5c9e",
+    "url": "/movie-app-2021/static/js/runtime-main.abcf39ef.js"
+  }
+]);
